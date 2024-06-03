604e1ed335b8c7986a5bae997fe8a815e491c3f7
s29143 <s29143@pjwstk.edu.pl>
2024-06-03 21:05:56 +0200

    added db context

---
+++ APBD9/Context/ApbdContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APBD9.Context;
+
+public class ApbdContext : DbContext
+{
+    protected ApbdContext()
+    {
+    }
+
+    public ApbdContext(DbContextOptions options) : base(options)
+    {
+    }
+}
